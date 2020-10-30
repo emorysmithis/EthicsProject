38b93174b405c268d940030c8ee18ee4eb691fef
emorysmithis <emorysmithis@gmail.com>
2020-10-30 12:14:42 -0400

    simple test

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-
+from game import test 
 @app.route("/")
 def main():
-    return render_template("index.html")
+    return test(15)
+    #return render_template("index.html")
 
 if __name__ == "__main__":
     app.run
