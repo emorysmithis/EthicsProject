f274abb62002dc65f0db5e71b82f91a17a6e5e55
emorysmithis <emorysmithis@gmail.com>
2020-10-31 16:50:54 -0400

    adding choices

--- flask_test/microblog/app/routes.py
+++ flask_test/microblog/app/routes.py
@@ -2,13 +2,27 @@ from flask import render_template, flash, redirect
 from app import app 
 from app.forms import LoginForm
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    posts = [ 
+        { 
+            'choice': {'choice':'A'}, 
+            'body': 'This is choice A!'
+        }, 
+        { 
+            'choice': {'choice': 'B'}, 
+            'body': 'This is choice B!'
+        }
+    ]
+
+
+
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}, choice_A={}, choice_B={}'.format(
             form.username.data, form.remember_me.data, form.choice_A.data, form.choice_B.data))
-        print("Login accepted. Redirecting to Home")
+        print("Pressed submit")
         return redirect('/index')
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html', title='Sign In', form=form, posts=posts) 
 
