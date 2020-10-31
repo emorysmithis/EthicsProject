84e0c56309384ffb63b7557e06a1c05ddb0ebe25
emorysmithis <emorysmithis@gmail.com>
2020-10-31 15:20:37 -0400

    adding choices

--- flask_test/microblog/app/forms.py
+++ flask_test/microblog/app/forms.py
@@ -6,4 +6,6 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
+    choice_A = BooleanField('Choice A') 
+    choice_B = BooleanField('Choice B') 
     submit = SubmitField('Sign In')
