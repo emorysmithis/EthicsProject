504acd120387eac239f835030e6c31e65026e2ce
emorysmithis <emorysmithis@gmail.com>
2020-10-31 17:22:19 -0400

    get rid of login stuff

--- flask_test/microblog/app/forms.py
+++ flask_test/microblog/app/forms.py
@@ -3,9 +3,6 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
     choice_A = BooleanField('Choice A') 
     choice_B = BooleanField('Choice B') 
-    submit = SubmitField('Sign In')
+    submit = SubmitField('Submit Answer')
