6c219e83572603e3b9942b39f5cd6695efce3666
Yeon Jun Kim <yeonjunkim99@gmail.com>
2020-11-02 19:31:50 -0500

    started welcome page and began importing content

--- flask_test/microblog/app/routes.py
+++ flask_test/microblog/app/routes.py
@@ -2,9 +2,17 @@ from flask import render_template, flash, redirect
 from app import app 
 from app.forms import LoginForm
 
+import json
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    with open("content.json") as file:
+        content = json.load(file)
+
     posts = [ 
         { 
             'choice': {'choice':'A'}, 
@@ -22,14 +30,14 @@ def login():
         flash("You pressed submit")
         if form.choice_A.data: 
             flash('You selected Choice A')  
-            return render_template('login.html', title='A', form=form, posts=posts)
+            return render_template('login.html', title='A', form=form, posts=posts, content=content)
             print('After showing A page') 
         elif form.choice_B.data: 
             flash('You selected Choice B') 
-            return render_template('login.html', title='B', form=form, posts=posts)   
+            return render_template('login.html', title='B', form=form, posts=posts, content=content)   
             print('After showing B page')
         else: 
-            return render_template('login.html', title='Amagon', form=form, posts=posts) 
+            return render_template('login.html', title='Amagon', form=form, posts=posts, content=content) 
     else: 
-        return render_template('login.html', title='Amagon', form=form, posts=posts) 
+        return render_template('login.html', title='Amagon', form=form, posts=posts, content=content) 
 
