deff16db85ce4ba593566bdf6ce80c6d66e42cbb
emorysmithis <emorysmithis@gmail.com>
2020-11-02 21:12:13 -0500

    demo

--- flask_test/microblog/app/routes.py
+++ flask_test/microblog/app/routes.py
@@ -8,43 +8,34 @@ import json
 def home():
     return render_template('index.html')
 
-i = -1
+i = 0
 
 @app.route('/adventure', methods=['GET', 'POST'])
 def login():
     global i 
     with open("content.json") as file:
         content = json.load(file)
+    #print(content)
 
-    posts = [ 
-        { 
-            'choice': {'choice':'A'}, 
-            'body': 'This is choice A!', 
-            'consequence': 'This is the consequence for Choice A'
-        }, 
-        { 
-            'choice': {'choice': 'B'}, 
-            'body': 'This is choice B!', 
-            'consequence': 'This is the consequence for Choice B'
-        }
-    ]
     form = LoginForm()
     numScenarios = len(content)
     print("Number of Scenarios: {}".format(numScenarios))
-    if form.validate_on_submit():
-        i+= 1 
-        print("on scenario {}".format(i)) 
+    if form.validate_on_submit(): 
         flash("You pressed submit")
         if form.choice_A.data: 
+            print("on scenario {}".format(i)) 
             flash('You selected Choice A')  
-            return render_template('adventure.html', title='A', form=form, posts=posts, content=content, i=i)
+            return render_template('adventure.html', title='A', form=form, content=content, i=i)
             print('After showing A page') 
         elif form.choice_B.data: 
+            print("on scenario {}".format(i)) 
             flash('You selected Choice B') 
-            return render_template('adventure.html', title='B', form=form, posts=posts, content=content, i=i)   
+            return render_template('adventure.html', title='B', form=form, content=content, i=i)   
             print('After showing B page')
         else: 
-            return render_template('adventure.html', title='Amagon', form=form, posts=posts, content=content, i=i) 
-    else: 
-        return render_template('adventure.html', title='Amagon', form=form, posts=posts, content=content, i=i) 
+            i += 1
+            return render_template('adventure.html', title='Amagon', form=form, content=content, i=i) 
+    else:
+        print("i on loading initial page:{}".format(i))
+        return render_template('adventure.html', title='Amagon', form=form, content=content, i=i) 
 
