6e78cd9e2be63de7667bd05ac3ac79f225cfe199
emorysmithis <emorysmithis@gmail.com>
2020-11-02 20:11:43 -0500

    for loop for scenarios

--- flask_test/microblog/app/routes.py
+++ flask_test/microblog/app/routes.py
@@ -8,8 +8,11 @@ import json
 def home():
     return render_template('index.html')
 
+i = -1
+
 @app.route('/adventure', methods=['GET', 'POST'])
 def login():
+    global i 
     with open("content.json") as file:
         content = json.load(file)
 
@@ -26,18 +29,22 @@ def login():
         }
     ]
     form = LoginForm()
+    numScenarios = len(content)
+    print("Number of Scenarios: {}".format(numScenarios))
     if form.validate_on_submit():
+        i+= 1 
+        print("on scenario {}".format(i)) 
         flash("You pressed submit")
         if form.choice_A.data: 
             flash('You selected Choice A')  
-            return render_template('adventure.html', title='A', form=form, posts=posts, content=content)
+            return render_template('adventure.html', title='A', form=form, posts=posts, content=content, i=i)
             print('After showing A page') 
         elif form.choice_B.data: 
             flash('You selected Choice B') 
-            return render_template('adventure.html', title='B', form=form, posts=posts, content=content)   
+            return render_template('adventure.html', title='B', form=form, posts=posts, content=content, i=i)   
             print('After showing B page')
         else: 
-            return render_template('adventure.html', title='Amagon', form=form, posts=posts, content=content) 
+            return render_template('adventure.html', title='Amagon', form=form, posts=posts, content=content, i=i) 
     else: 
-        return render_template('adventure.html', title='Amagon', form=form, posts=posts, content=content) 
+        return render_template('adventure.html', title='Amagon', form=form, posts=posts, content=content, i=i) 
 
