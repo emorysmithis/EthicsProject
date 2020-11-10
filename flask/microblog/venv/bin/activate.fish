073b09655c1e693baa6fcb5f404e2c6a111d4729
emorysmithis <emorysmithis@gmail.com>
2020-11-09 21:28:21 -0500

    venv

--- flask/microblog/venv/bin/activate.fish
+++ flask/microblog/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/emorysmithis/flask_test/microblog/venv"
+set -gx VIRTUAL_ENV "/home/emorysmithis/EthicsProject/flask/microblog/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
