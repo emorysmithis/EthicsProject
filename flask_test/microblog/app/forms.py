10649ff768a6a1e3cb238984c5467cce0fa5cf7f
emorysmithis <emorysmithis@gmail.com>
2020-11-09 20:43:39 -0500

    remove old

--- flask_test/microblog/app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-    choice_A = BooleanField('Choice A') 
-    choice_B = BooleanField('Choice B') 
-    submit = SubmitField('Submit Answer')
-    nextq = SubmitField('Next Question') 
